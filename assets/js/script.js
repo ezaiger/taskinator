1eea3f885c669864c7f1ae631953c9b830ab29cc
EZaiger <elisabeth.zaiger@oit.edu>
2022-06-04 15:23:41 -0700

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.dir(window.document);
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+/* REFACTORED CODE */
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item"; // creates new task item
+    listItemEl.textContent = "This is a new task."; // styles new task item
+    tasksToDoEl.appendChild(listItemEl); // appends element to task list
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
