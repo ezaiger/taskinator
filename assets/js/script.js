c20eccf7612e0b9dd52d1638beb4111e4e1c2099
EZaiger <elisabeth.zaiger@oit.edu>
2022-06-04 16:27:48 -0700

    add form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 /* REFACTORED CODE */
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item"; // creates new task item
     listItemEl.textContent = "This is a new task."; // styles new task item
     tasksToDoEl.appendChild(listItemEl); // appends element to task list
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
